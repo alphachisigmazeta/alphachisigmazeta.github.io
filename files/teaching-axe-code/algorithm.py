5c93cf2f8bcab1eeed19379291c20ed54a39eef4
Caleb Ju <calebju@gmail.com>
2018-10-30 14:52:54 -0500

    zip file

--- files/teaching-axe-code/algorithm.py
+++
@@ -1,8 +0,0 @@
-import random
-
-def decideToBuyOrSell(price):
-    if(random.randint(0,100) % 4 == 1):
-        return 'buy'
-    elif(random.randint(0,100) % 25 == 0):
-        return 'sell'
-    return ''
